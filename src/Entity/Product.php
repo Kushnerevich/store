fb0d8ba34c795cff08f42b740b2befb6f6e6785f
Kushnerevich Pavel <p.kushnerevich@itransition.com>
2018-07-27 18:08:27 +0300

    Adding Product class

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -15,4 +15,24 @@ class Product
     public $imageUrl;
     public $count;
     public $description;
+
+    private $password;
+    private $host;
+    private $username;
+    private $conn;
+
+    public function __construct()
+    {
+        $creditals = new Creditals();
+        $this->password = $creditals->getCreditals()->passwordDb;
+        $this->username = $creditals->getCreditals()->username;
+        $this->host = $creditals->getCreditals()->hostDB;
+        $this->conn = mysqli_connect($this->host, $this->username, $this->password);
+    }
+
+    public function getProduct()
+    {
+        return $this;
+    }
+
 }
